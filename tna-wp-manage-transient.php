d45bae6aa0709aba015f75897d092700bcb39c76
domingobishop <domingobishop@gmail.com>
2020-07-14 14:16:59 +0100

    first commit

---
+++ tna-wp-manage-transient.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: TNA WP Manage Transient
+ * Plugin URI: https://github.com/nationalarchives/tna-wp-manage-transient
+ * Description: The National Archives plugin.
+ * Version: 1.0
+ * Author: Chris Bishop
+ * Author URI: https://github.com/nationalarchives
+ * License: GPL2
+ */
+
+
